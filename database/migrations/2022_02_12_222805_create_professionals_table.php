891adee552eac50f4176f3435588a64f3ca4919f
loro102 <loro102@gmail.com>
2022-02-12 23:36:42 +0100

    Clientes,expedientes y profesionales

--- database/migrations/2022_02_12_222805_create_professionals_table.php
+++ database/migrations/2022_02_12_222805_create_professionals_table.php
@@ -1,47 +1,1 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('professionals', function (Blueprint $table) {
-            $table->id();
-            $table->integer('group_id')->unsigned();
-            $table->string('Nombre')->nullable();
-            $table->string('num_colegiado')->nullable();
-            $table->string('nif')->nullable();
-            $table->string('especialidad')->nullable();
-            $table->string('direccion')->nullable();
-            $table->string('localidad')->nullable();
-            $table->string('codigo_postal')->nullable();
-            $table->string('provincia')->nullable();
-            $table->string('email')->nullable();
-            $table->string('iban')->nullable();
-            $table->text('notas')->nullable();
-            $table->text('acuerdo_pago')->nullable();
-            $table->boolean('indemnizacion')->nullable();
-            $table->boolean('homologado')->nullable();
-            $table->boolean('activo')->nullable();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('professionals');
-    }
-};
+<?phpuse Illuminate\Database\Migrations\Migration;use Illuminate\Database\Schema\Blueprint;use Illuminate\Support\Facades\Schema;return new class extends Migration{    /**     * Run the migrations.     *     * @return void     */    public function up()    {        Schema::create('professionals', function (Blueprint $table) {            $table->id();            $table->integer('group_id')->unsigned();            $table->string('Nombre')->nullable();            $table->string('num_colegiado')->nullable();            $table->string('nif')->nullable();            $table->string('especialidad')->nullable();            $table->string('direccion')->nullable();            $table->string('localidad')->nullable();            $table->string('codigo_postal')->nullable();            $table->string('provincia')->nullable();            $table->string('email')->nullable();            $table->string('iban')->nullable();            $table->text('notas')->nullable();            $table->text('acuerdo_pago')->nullable();            $table->boolean('indemnizacion')->nullable();            $table->boolean('homologado')->nullable();            $table->boolean('activo')->nullable();            $table->timestamps();        });    }    /**     * Reverse the migrations.     *     * @return void     */    public function down()    {        Schema::dropIfExists('professionals');    }};
